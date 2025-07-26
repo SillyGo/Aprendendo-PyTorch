3917e35ff1025a554d413bf1b6624ae628e57123
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2025-07-26 18:20:25 -0300

    Update potholeMLP.py
    versão final, acurácia mt massa

--- potholeMLP.py
+++ potholeMLP.py
@@ -3,6 +3,8 @@ import pandas as pd
 import torch
 import torch.nn as nn
 
+#TRIP 2:
+
 data_in = pd.read_csv('trip2_potholes.csv')
 data_out= pd.read_csv('trip2_sensors.csv')
 
@@ -21,8 +23,78 @@ for i in range(len(time_stamp)):
     labels.append(0)
 
 
-labels = np.array(labels)
+labels1 = np.array(labels)
+acc1 = np.array(acc)
+
+#TRIP 3:
+
+data_in = pd.read_csv('trip3_potholes.csv')
+data_out= pd.read_csv('trip3_sensors.csv')
+
+sensor_data = data_out.sort_values('timestamp', ascending=True)
+acc = sensor_data['accelerometerY'] #acho que isso é suficiente para pegar as informações do acelerometro y, testarei isso agora.
+
+time_stamp = [x for x in range(2210)]
+pothole_data = data_in.sort_values('timestamp', ascending=True)['timestamp'].values # Extract the 'timestamp' column and get the values as a NumPy array
+for i in range(len(pothole_data)): # Iterate through the array
+  pothole_data[i] = int((pothole_data[i] - 1493002313.5)*5)
+
+labels = []
+for i in range(len(time_stamp)):
+  if (time_stamp[i] in pothole_data):
+    labels.append(1)
+  else:
+    labels.append(0)
+
+labels2 = np.array(labels)
+acc2 = np.array(acc)
+
+#TRIP 4:
+
+data_in = pd.read_csv('trip4_potholes.csv')
+data_out= pd.read_csv('trip4_sensors.csv')
+
+sensor_data = data_out.sort_values('timestamp', ascending=True)
+acc = sensor_data['accelerometerY'] #acho que isso é suficiente para pegar as informações do acelerometro y, testarei isso agora.
+
+time_stamp = [x for x in range(len(acc))]
+pothole_data = data_in.sort_values('timestamp', ascending=True)['timestamp'].values # Extract the 'timestamp' column and get the values as a NumPy array
+for i in range(len(pothole_data)): # Iterate through the array
+  pothole_data[i] = int((pothole_data[i] - 1493002780.6)*5)
+
+labels = []
+for i in range(len(time_stamp)):
+  if (time_stamp[i] in pothole_data):
+    labels.append(1)
+  else:
+    labels.append(0)
+
+labels3 = np.array(labels)
+acc3 = np.array(acc)
+
+#COLANDO AS DIFERENTES TRIPS EM UM SÓ:
+
+labels = []
+acc = []
+for i in range(len(labels1)):
+  labels.append(labels1[i])
+  acc.append(acc1[i])
+
+for i in range(len(labels2)):
+  labels.append(labels2[i])
+  acc.append(acc2[i])
+
+for i in range(len(labels3)):
+  labels.append(labels3[i])
+  acc.append(acc3[i])
+
 acc = np.array(acc)
+labels = np.array(labels)
+
+print(len(acc))
+print(len(labels))
+
+#CRIANDO AS IMAGENS:
 
 def create_sequences(data, seq_length):
     xs = []
@@ -45,27 +117,27 @@ def label_image(labels, seq_lenght):
         out.append(0)
   return out
 
-seq = 20
+
+seq = 50
 xs = create_sequences(acc, seq)
 ys = label_image(labels, seq)
 
-print("---")
+print(len(xs[0]))
+
 counter = 0
 for i in range(len(ys)):
   if ys[i] == 1:
     counter = counter + 1
-    print(i)
 print("---")
-print(f'{100*(counter/seq)}%')
+print(f'{100*(counter/len(ys))}%')
 print("---")
+
 trainX = torch.tensor(xs, dtype=torch.float32)
 trainY = torch.tensor(ys, dtype=torch.float32)
 
-print(len(trainX))
-print(trainY.shape)
-
 training_set = torch.utils.data.TensorDataset(trainX, trainY)
-print(training_set)
+
+#criando o modelo
 
 class NeuralNet(nn.Module): 
     def __init__(self, in_size, hide_size, out_size): #define o geral da rede, ou seja, sua arquitetura e funções de ativação
@@ -82,10 +154,10 @@ class NeuralNet(nn.Module):
     
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-in_size = 99
+in_size = 115
 hide_size = 128
 num_classes = 2
-num_epochs = 100000
+num_epochs = 20000
 batch_size = 20
 lrate = 0.001
 
@@ -95,6 +167,9 @@ print(train_loader)
 model = NeuralNet(in_size=in_size, hide_size=hide_size, out_size=num_classes)
 criterion = nn.CrossEntropyLoss() #novamente, aplica o softmax por nos
 optimizer = torch.optim.SGD(model.parameters(), lr=lrate)
+
+
+#treinamento;
 
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
@@ -113,4 +188,52 @@ for epoch in range(num_epochs):
         optimizer.step()
 
     if epoch % 1000 == 0:
-        print(f'epoch: {epoch}, / loss: {loss.item()}')
+      print(f'epoch: {epoch}, / loss: {loss.item()}')
+    elif epoch == num_epochs - 1:
+      print(f'epoch: {epoch}, / loss: {loss.item()}')
+
+
+#verif
+
+data_in = pd.read_csv('trip4_potholes.csv')
+data_out= pd.read_csv('trip4_sensors.csv')
+
+sensor_data = data_out.sort_values('timestamp', ascending=True)
+acc = sensor_data['accelerometerY'] #acho que isso é suficiente para pegar as informações do acelerometro y, testarei isso agora.
+
+time_stamp = [x for x in range(len(acc))]
+pothole_data = data_in.sort_values('timestamp', ascending=True)['timestamp'].values # Extract the 'timestamp' column and get the values as a NumPy array
+for i in range(len(pothole_data)): # Iterate through the array
+  pothole_data[i] = int((pothole_data[i] - 1493003562.6)*5)
+
+labels = []
+for i in range(len(time_stamp)):
+  if (time_stamp[i] in pothole_data):
+    labels.append(1)
+  else:
+    labels.append(0)
+
+labels4 = np.array(labels)
+acc4 = np.array(acc)
+
+
+test_set = torch.utils.data.TensorDataset(trainX, trainY)
+
+test_loader  = torch.utils.data.DataLoader(dataset=test_set, 
+                                           batch_size=batch_size, 
+                                           shuffle=False)
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+
+        #torch.max() retorna o valor e o index, estamos interessados somente no index, de modo que salvamos o outro numa variavel _, usada para indicar que não usaremos o valor.
+        _, predcits = torch.max(outputs,1)
+        n_samples += labels.shape[0] #numero de samples para uma batch, deve retornar 100
+        n_correct += (predcits == labels).sum().item()
+
+    acc = 100.0*(n_correct/n_samples)
+    print(f'acc: {acc}%')
