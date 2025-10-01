11e09ab3e32163429223df3ebf0eec79baf5d67b
SillyGo <arthurmoraisvillar@gmail.com>
2025-10-01 14:56:18 -0300

    dropout layers

--- torch_cnn1.py
+++ torch_cnn1.py
@@ -5,27 +5,26 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 import torchvision
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 class convnet(nn.Module):
     def __init__(self):
         super(convnet, self).__init__()
+        self.lin_dropout = nn.Dropout(p=0.2)
+        self.cnn_dropout = nn.Dropout2d(p=0.2)
         self.conv1 = nn.Conv2d(3,6,5) #aqui, devemos especificar o tamanho dos canais de cor (nesse caso, 3), o tamanho do output fica como escolha do usuário, e o tamanho de kernel, aqui, 5
         self.pool = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(6,16,5) #novamente, o tamanho do kernel fica o mesmo, e o tamanho do output aumenta
-        self.fc1 = nn.Linear(16*5*5, 120) #temos 16*5*5 logo que o tamanho final da imagem, depois de todas as transformações impostas pelas camadas de convolução e de maxpooling, será 5x5
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10) #nos temos 10 classes diferentes, logo, deveremos ter 10 outputs.
+        self.fc1 = nn.Linear(16*5*5, 124) #temos 16*5*5 logo que o tamanho final da imagem, depois de todas as transformações impostas pelas camadas de convolução e de maxpooling, será 5x5
+        self.fc2 = nn.Linear(124, 64)
+        self.fc3 = nn.Linear(64, 10) #nos temos 10 classes diferentes, logo, deveremos ter 10 outputs.
 
     def forward(self, x):
-        x = self.pool(functional.relu(self.conv1(x)))
-        x = self.pool(functional.relu(self.conv2(x)))
+        x = self.pool(self.cnn_dropout(functional.relu(self.conv1(x))))
+        x = self.pool(self.cnn_dropout(functional.relu(self.conv2(x))))
        
         x = x.view(-1, 16*5*5)
-        x = functional.relu(self.fc1(x))
-        x = functional.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.lin_dropout(functional.relu(self.fc1(x)))
+        x = self.lin_dropout(functional.relu(self.fc2(x)))
+        x = self.lin_dropout(self.fc3(x))
         
         return x
 
@@ -34,13 +33,20 @@ class convnet(nn.Module):
 #configuração de dispositivo
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-num_epochs = 4
+num_epochs = 15
 batch_size = 4
 lrate = 0.001
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
+transform = transforms.Compose(         #carrega os dados e aplica o data augmentation
+    [
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(degrees=45),
+        transforms.RandomVerticalFlip(p=0.05),
+        transforms.ColorJitter(brightness=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+     ]
+)
 
 train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 test_data =  torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
