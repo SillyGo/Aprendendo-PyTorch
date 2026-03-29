f9763aa6e863ef7b7ea7cc62308e60b761dcc68a
SillyGo <arthurmoraisvillar@gmail.com>
2026-03-29 16:35:27 -0300

    [DEV] depth anything - openCV webcam stream pipeline

--- ObjDetection/depthAnythingBasics.py
+++ ObjDetection/depthAnythingBasics.py
@@ -1,41 +1,46 @@
 from transformers import AutoImageProcessor, AutoModelForDepthEstimation
 import torch
-import numpy as np
 from PIL import Image
-import httpx
-from io import BytesIO
 import matplotlib.pyplot as plt
 import warnings
+import numpy as np
+import cv2
 
 warnings.filterwarnings('ignore')
-
-image = Image.open('car1.png').convert("RGB")
-
-plt.imshow(image)
-plt.axis('off') 
-plt.show()
 
 image_processor = AutoImageProcessor.from_pretrained("LiheYoung/depth-anything-small-hf")
 model = AutoModelForDepthEstimation.from_pretrained("LiheYoung/depth-anything-small-hf")
 
-# prepare image for the model
-inputs = image_processor(images=image, return_tensors="pt")
+def RunDepthEstimation(image):
+    inputs = image_processor(images=image, return_tensors="pt")
 
-with torch.no_grad():
-    outputs = model(**inputs)
+    with torch.no_grad():
+        outputs = model(**inputs)
 
-# interpolate to original size
-post_processed_output = image_processor.post_process_depth_estimation(
-    outputs,
-    target_sizes=[(image.height, image.width)],
-)
+    post_processed_output = image_processor.post_process_depth_estimation(
+        outputs,
+        target_sizes=[(image.height, image.width)],
+    )
 
-# visualize the prediction
-predicted_depth = post_processed_output[0]["predicted_depth"]
-depth = predicted_depth * 255 / predicted_depth.max()
-depth = depth.detach().cpu().numpy()
-depth = Image.fromarray(depth.astype("uint8"))
+    predicted_depth = post_processed_output[0]["predicted_depth"]
+    depth = predicted_depth * 255 / predicted_depth.max()
+    depth = depth.detach().cpu().numpy()
 
-plt.imshow(depth)
-plt.axis('off') 
-plt.show()
+    return Image.fromarray(depth.astype("uint8"))
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    depth = np.array(RunDepthEstimation(Image.fromarray(frame)))
+
+    cv2.imshow('Profundidade monocular da webcam', depth)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
